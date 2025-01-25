0fabfe9855ce01ed9183d275cb38fc54f0c2f58b
Victor Su <susuvictorvictor@gmail.com>
2025-01-25 15:04:58 -0500

    d

--- website/script.js
+++ website/script.js
@@ -1,3 +1,24 @@
+
+
+
+
+
+
+
+let crypto = 0;
+let rate = 1;
+
+function mineCrypto() {
+    crypto += rate;
+    document.getElementById("crypto-balance").textContent = crypto.toFixed(2) + " ₿";
+}
+
+function reset() {
+    crypto = 0;
+    document.getElementById("crypto-balance").textContent = crypto.toFixed(2) + " ₿";
+}
+
+
 var fetchUrl = require("fetch").fetchUrl;
 const link = "https://api.coinpaprika.com/v1";
 
@@ -10,20 +31,6 @@ fetchUrl(`https://api.coinpaprika.com/v1/coins/btc-bitcoin/ohlcv/today`, functio
 
 document.cookie = `{ "coins:": []}`;
 
-
-
-let crypto = 0;
-let rate = 1;
-
-function mineCrypto() {
-    crypto += rate;
-    document.getElementById("crypto-balance").textContent = crypto.toFixed(2) + " ₿";
-     }
-
-function reset() {
-    crypto = 0;
-    document.getElementById("crypto-balance").textContent = crypto.toFixed(2) + " ₿";
-    }
 
 
     
