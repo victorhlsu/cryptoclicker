f89306d5b6e2559f1d568de8374c50bcefedebb0
Victor Su <susuvictorvictor@gmail.com>
2025-01-25 16:01:41 -0500

    more

--- website/script.js
+++ website/script.js
@@ -1,35 +1,36 @@
+var cookie;
 
 
+function initializeCookie() {
+     if (!document.cookie) {
+          document.cookie = `{"coins": {"btc": 5}}`;
+     }
+     cookie = JSON.parse(document.cookie);
+     document.getElementById("crypto-balance").textContent = cookie.coins["btc"].toFixed(2) + " ₿";
+}
 
-
-
-
-
-let crypto = 0;
-let rate = 1;
 
 function mineCrypto() {
-    crypto += rate;
-    document.getElementById("crypto-balance").textContent = crypto.toFixed(2) + " ₿";
+     cookie.coins.btc += 1;
+    document.getElementById("crypto-balance").textContent = cookie.coins.btc.toFixed(2) + " ₿";
 }
 
 function reset() {
-    crypto = 0;
-    document.getElementById("crypto-balance").textContent = crypto.toFixed(2) + " ₿";
+     cookie.coins.btc = 0;
+    document.getElementById("crypto-balance").textContent = cookie.coins.btc.toFixed(2) + " ₿";
+}
+
+function saveCookie() {
+     document.cookie = JSON.stringify(cookie);
 }
 
 
-var fetchUrl = require("fetch").fetchUrl;
-const link = "https://api.coinpaprika.com/v1";
+// var fetchUrl = require("fetch").fetchUrl;
+// const link = "https://api.coinpaprika.com/v1";
 
+// fetchUrl(`https://api.coinpaprika.com/v1/coins/btc-bitcoin/ohlcv/today`, function (err, meta, body) {
+//      (JSON.parse(body.toString())[0].open)})
 
-
-
-fetchUrl(`https://api.coinpaprika.com/v1/coins/btc-bitcoin/ohlcv/today`, function (err, meta, body) {
-     (JSON.parse(body.toString())[0].open)})
-
-
-document.cookie = `{ "coins:": []}`;
 
 
 
