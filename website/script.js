a460d2b1818d8c1fdd2fdb7301e45da3acb66831
Victor Su <susuvictorvictor@gmail.com>
2025-01-25 14:57:33 -0500

    hi

--- website/script.js
+++ website/script.js
@@ -3,7 +3,6 @@ const link = "https://api.coinpaprika.com/v1";
 
 
 
-s
 
 fetchUrl(`https://api.coinpaprika.com/v1/coins/btc-bitcoin/ohlcv/today`, function (err, meta, body) {
      (JSON.parse(body.toString())[0].open)})
