351b5acaeb801bcd52c518920dc30dd245b565c6
Victor Su <susuvictorvictor@gmail.com>
2025-01-25 15:02:27 -0500

    d

--- website/script.js
+++ website/script.js
@@ -12,6 +12,22 @@ document.cookie = `{ "coins:": []}`;
 
 
 
+let crypto = 0;
+let rate = 1;
+
+function mineCrypto() {
+    crypto += rate;
+    document.getElementById("crypto-balance").textContent = crypto.toFixed(2) + " ₿";
+     }
+
+function reset() {
+    crypto = 0;
+    document.getElementById("crypto-balance").textContent = crypto.toFixed(2) + " ₿";
+    }
+
+
+    
+
 
 
 
