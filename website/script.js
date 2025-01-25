38c4a8df60d726c2fda988cc0e100e01ca32b4ea
Victor Su <susuvictorvictor@gmail.com>
2025-01-25 16:50:36 -0500

    ifeo

--- website/script.js
+++ website/script.js
@@ -1,24 +1,57 @@
+// Change These
+
+var moneyPerClick = 1;
+
+// 
+
+
 var cookie;
+var currCoin = ["Bitcoin", "btc", "₿", "btc-bitcoin"];
+var exchangeRate = 104997.80;
+
+var clickAmount = moneyPerClick/exchangeRate
 
 
 function initializeCookie() {
-     if (!document.cookie) {
-          document.cookie = `{"coins": {"btc": 5}}`;
+     //getUSDPrice("btc").then(x => {exchangeRate = x;})
+     document.getElementById("earning-rate").textContent = moneyPerClick + "$ USD";
+     document.cookie = '';
+     if (!document.cookie || document.cookie == '' || document.cookie == 'undefined') {
+          document.cookie = `{"coins": {"btc": 0, "eth": 0}}`;
      }
+     
      cookie = JSON.parse(document.cookie);
-     document.getElementById("crypto-balance").textContent = cookie.coins["btc"].toFixed(2) + " ₿";
+     updateBalance()
+}
+
+async function getUSDPrice(coin) {
+     url = "https://api.coinpaprika.com/v1/coins/btc-bitcoin/ohlcv/today";
+     try {
+          const response = await fetch(url);
+          const json = await response.json();
+          return json[0].close * cookie.coins[currCoin[1]];
+        } catch (error) {return 0;}
 }
 
 
 function mineCrypto() {
-     cookie.coins.btc += 1;
-    document.getElementById("crypto-balance").textContent = cookie.coins.btc.toFixed(2) + " ₿";
+     cookie.coins[currCoin[1]] += clickAmount;
+     updateBalance();
 }
 
 function reset() {
-     cookie.coins.btc = 0;
-    document.getElementById("crypto-balance").textContent = cookie.coins.btc.toFixed(2) + " ₿";
+     cookie.coins[currCoin[1]] = 0
+     updateBalance();
 }
+
+
+function updateBalance() {
+     document.getElementById("crypto-balance").textContent = cookie.coins[currCoin[1]].toFixed(10) + ` ${currCoin[2]}`;
+     document.getElementById("usdprice").textContent = (exchangeRate*cookie.coins[currCoin[1]]).toFixed(2);
+
+}
+
+
 
 function saveCookie() {
      document.cookie = JSON.stringify(cookie);
